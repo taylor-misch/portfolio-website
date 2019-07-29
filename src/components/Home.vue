5a85985162604e0009efdbe64f2b1a69399c6b41
Taylor <taylormisch@gmail.com>
2019-07-29 07:39:33 -0500

    working on parallax effect

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="home">
+    <!-- <div class="pimg1">
+      <div class="ptext">
+        <span class="border">Parallax Image</span>
+      </div>
+    </div>-->
     <div id="dimmed">
       <v-parallax id="background" height="1500" src="../assets/background.jpg">
-        <h1 id="heading">Welcome. Have a look around.</h1>
+        <!-- <h1 id="heading">Welcome. Have a look around.</h1> -->
       </v-parallax>
     </div>
   </div>
@@ -15,17 +20,36 @@ export default {
 </script>
 
 <style scoped>
-#heading {
+/* #home {
+  background-color: black;
+}
+.pimg1 {
+  position: relative;
+  opacity: 0.3;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  background-attachment: fixed;
+}
+.pimg1 {
+  background-image: url("../assets/background.jpg");
+  min-height: 1200px;
+} */
+/* #heading {
   color: red;
   align-self: auto;
 }
 #dimmed {
-  background-color: red;
-  opacity: 90%;
+  opacity: 0.3;
   z-index: 200;
-}
+} 
 v-parallax {
   z-index: -1;
-}
+  position: relative;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+} */
 </style>
 
