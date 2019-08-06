4f53151590b4dc4ab9d113a4eed6b51e3385fdf0
Taylor <taylormisch@gmail.com>
2019-08-06 07:32:55 -0500

    cleaned up projects section, changed about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,19 +1,24 @@
 <template>
   <Module id="about" title="About">
-    <div class="headline">
-      Taylor is a software developer. He graduated from the University of Wisconsin - Eau Claire in 2019 with a degree in Computer Science. Throughout his college career he learned the fundementals of programming through his schoolwork, the inner-workings of business through his internships, and how to think entreneaurally during his time as an original member of Clearwater Labs (a student run software development company startup) during his senior year.
-      This website features some his professional experinece, projects, and interests. Feel free to look around, explore his GitHub, or reach out to him on LinkedIn.
-    </div>
-    <v-layout justify-center >
-      <v-responsive max-width="750">
-        <v-carousel>
-          <v-carousel-item
-            v-for="image in images"
-            :key="image.id"
-            :src="require('@/assets/'+image.src)"
-          ></v-carousel-item>
-        </v-carousel>
-      </v-responsive>
+    <v-layout xs12 md6>
+      <v-flex class="headline text-left">
+        <p>Hi, I'm Taylor Misch. This site has been a bit of a passion project of mine for a long time now. As a software developer I've always though it is a rite of passage to create your own website - so here's mine.</p>
+        <p>On this site I post projects that I've worked on (inside and outside the realm of software development), things I'm interested in, and pictures from my latest adventure - whatever that may be.</p>
+        <p>Feel free to look around or reach out to me if you think I can help you in anyway or just would like to talk about something you see here.</p>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12 md6>
+        <v-responsive aspect-ratio="16/9">
+          <v-carousel>
+            <v-carousel-item
+              v-for="image in images"
+              :key="image.id"
+              :src="require('@/assets/carousel/'+image.src)"
+            ></v-carousel-item>
+          </v-carousel>
+        </v-responsive>
+      </v-flex>
     </v-layout>
   </Module>
 </template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,11 +3,15 @@
     <div class="pimg1">
       <v-container fill-height>
         <v-layout column fill-height align-center justify-center>
-          <v-flex xs1 class="display-4 text-center font-weight-medium white--text">Taylor Misch</v-flex>
-          <v-flex
-            xs1
-            class="display-1 text-center font-weight-medium white--text"
-          >Full Stack Developer</v-flex>
+          <v-flex pb-6 xs1 class="display-4 text-center font-weight-medium white--text">Taylor Misch</v-flex>
+          <v-divider></v-divider>
+          <v-flex xs1 class="display-1 text-center font-weight-medium">
+            <span class="blue--text text--lighten-3">Full Stack Developer</span>
+            <br />
+            <span class="amber--text text--lighten-3">Hobbiest Musician</span>
+            <br />
+            <span class="red--text text--lighten-3">Lifelong Learner</span>
+          </v-flex>
         </v-layout>
       </v-container>
     </div>
