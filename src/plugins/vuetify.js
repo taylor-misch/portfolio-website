d0de36cd690fe74953c5c8fa4fdb10a544ba436e
Taylor <taylormisch@gmail.com>
2019-08-15 06:32:17 -0500

    Add theme template in vuetify for app consistency

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,26 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: 'mdi',
-  },
+    iconfont: "mdi"
+  }
+  // theme: {
+  //   themes: {
+  //     light: {
+  // primary: "#1976D2",
+  // secondary: "#424242",
+  // accent: "#82B1FF",
+  // error: "#FF5252"
+  //     },
+  //     dark: {
+  //        primary: '#1976D2',
+  // secondary: '#424242',
+  // accent: '#82B1FF',
+  // error: '#FF5252',
+  //     }
+  //   }
+  // }
 });
