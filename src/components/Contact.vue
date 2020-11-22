518d9bfe96c993fe063365189b893ca0d1259802
Taylor <taylormisch@gmail.com>
2020-11-21 20:50:28 -0600

    Added alert message to appear when a mesage is sent

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,44 +1,49 @@
 <template>
   <Module id="contact" title="Contact Me">
-    <v-form>
-      <v-row>
-        <v-col cols="12" md="6">
-          <v-row dense class="flex flex-column justify-center">
-            <v-col cols="12">
-              <v-text-field
-                v-model="params.fromName"
-                filled
-                background-color="white"
-                label="Name"
-                placeholder="John Doe"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                v-model="params.replyEmail"
-                filled
-                background-color="white"
-                label="Email"
-                placeholder="johndoe@gmail.com"
-              ></v-text-field>
-            </v-col>
-          </v-row>
+    <v-col v-if="alert" cols="12" md="5">
+      <v-alert v-model="alert" :type="messageStatus">
+        <v-row no-gutters
+          ><v-col class="grow">{{ alertMessage }}</v-col>
+          <v-col class="shrink">
+            <v-btn @click="alert = false">Send another message</v-btn>
+          </v-col></v-row
+        ></v-alert
+      >
+    </v-col>
+    <v-form v-else>
+      <v-row dense>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="params.fromName"
+            filled
+            background-color="white"
+            label="Name"
+          ></v-text-field>
         </v-col>
-        <v-col cols="12" md="6">
-          <v-row dense class="flex flex-column justify-center">
-            <v-col cols="12">
-              <v-textarea
-                v-model="params.message"
-                filled
-                background-color="white"
-                label="Message"
-                placeholder="Hey Taylor, love the website!"
-              ></v-textarea>
-            </v-col>
-          </v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="params.replyEmail"
+            filled
+            background-color="white"
+            label="Email"
+          ></v-text-field>
         </v-col>
       </v-row>
-      <v-btn color="blue" @click="sendEmail">Submit</v-btn>
+      <v-row dense>
+        <v-col cols="12" md="8">
+          <v-textarea
+            v-model="params.message"
+            filled
+            background-color="white"
+            label="Message"
+          ></v-textarea>
+        </v-col>
+      </v-row>
+      <v-row dense>
+        <v-col cols="12" md="8">
+          <v-btn color="blue" @click="sendEmail">Submit</v-btn>
+        </v-col>
+      </v-row>
     </v-form>
   </Module>
 </template>
@@ -59,6 +64,9 @@ export default {
         fromName: "",
         replyEmail: "",
       },
+      messageStatus: "info",
+      alert: false,
+      alertMessage: "",
     };
   },
   methods: {
@@ -72,12 +80,18 @@ export default {
         )
         .then(
           (result) => {
+            this.messageStatus = "success";
+            this.alertMessage = "Message Sent";
             // eslint-disable-next-line
             console.log("SUCCESS!", result.status, result.text);
+            this.alert = true;
           },
           (error) => {
+            this.messageStatus = "error";
+            this.alertMessage = "Something went wrong";
             // eslint-disable-next-line
             console.log("FAILED...", error);
+            this.alert = true;
           }
         );
     },
