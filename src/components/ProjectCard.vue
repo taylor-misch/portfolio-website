34ee1b3f91f430320ee78bc2a81ec715c72738ec
TMisch <taylormisch@gmail.com>
2020-11-24 21:00:13 -0600

    Add hover to projects

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,18 +1,24 @@
 <template>
-  <v-card color="grey lighten-4" class="text-xs-center ma-3 rounded-lg">
-    <v-tooltip bottom color="black">
-      <template v-slot:activator="{ on }">
-        <v-img
-          :src="require('@/assets/projects/' + project.logo)"
-          aspect-ratio="1"
-          :alt="project.alt"
-          v-on="on"
-          class="rounded-lg"
-        ></v-img>
-      </template>
-      <span>{{ project.name }}</span>
-    </v-tooltip>
-  </v-card>
+  <v-hover v-slot="{ hover }">
+    <v-card
+      :elevation="hover ? 16 : 2"
+      color="grey lighten-4"
+      class="text-xs-center ma-3 rounded-lg"
+    >
+      <v-tooltip bottom color="black">
+        <template v-slot:activator="{ on }">
+          <v-img
+            :src="require('@/assets/projects/' + project.logo)"
+            aspect-ratio="1"
+            :alt="project.alt"
+            v-on="on"
+            class="rounded-lg"
+          ></v-img>
+        </template>
+        <span>{{ project.name }}</span>
+      </v-tooltip>
+    </v-card>
+  </v-hover>
 </template>
 
 <script>
